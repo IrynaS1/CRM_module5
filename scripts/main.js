024f5392b8e94036cae0a2d7ef374f47ad5aff2f
IrynaS1 <snowqueen-2007@mail.ru>
2023-04-14 20:04:58 +0300

    changed index_cms.html,card and overlay (css), deleted wrapper (css), add 3 addEventListener for closing modal window

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const displayModal = document.querySelector('.overlay');
-
-displayModal.classList.add('.overlay__flex');
+const overlay = document.querySelector('.overlay');
 
 const mainTitle = document.querySelector('.title__text');
 
@@ -16,3 +14,21 @@ const checkboxDiscont =
 const discontText = document.querySelector('.form__input-line_discont');
 
 const totalCount = document.querySelector('.total__count-text_color');
+
+const buttonAddToCard = document.querySelector('.cms-order');
+
+buttonAddToCard.addEventListener('click', () => {
+	overlay.classList.add('overlay__flex');
+});
+
+const closeBtn = document.querySelector('.close__btn');
+
+closeBtn.addEventListener('click', () => {
+	overlay.classList.remove('overlay__flex');
+});
+
+overlay.addEventListener('click', () => {
+	overlay.classList.remove('overlay__flex');
+});
+
+
