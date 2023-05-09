1170adbcb1a86c6770279a39efcc568fbf987c8b
IrynaS1 <snowqueen-2007@mail.ru>
2023-05-09 18:53:05 +0300

    add modules

---
+++ scripts/modules/getElements.js
@@ -0,0 +1,51 @@
+const overlay = document.querySelector('.overlay');
+
+const mainTitle = document.querySelector('.title__text');
+
+const idOfGood = document.querySelector('.title__id-good');
+
+const formOrder = document.querySelector('.card-box');
+
+const checkboxDiscont =
+	document.querySelector('.form__input-line_discont-checkbox');
+
+const discontText = document.querySelector('.form__input-line_discont');
+
+const totalCount = document.querySelector('.total__count-value');
+
+const buttonAddToCard = document.querySelector('.cms-order');
+
+const closeBtn = document.querySelector('.close__btn');
+
+const table = document.querySelector('.data-table');
+
+const addGood = document.querySelector('.card-order');
+
+const newGoodCount = document.querySelector('.card-order');
+
+const sumEl = document.querySelectorAll('.data-table__item-sum');
+
+const totalCountLine = document.querySelector('.total__count-text__sum');
+
+const headTr = document.querySelector('.data-table__head-items');
+
+const totalCountMulti = document.querySelector('.total__count-value');
+
+export {
+	overlay,
+	mainTitle,
+	idOfGood,
+	formOrder,
+	checkboxDiscont,
+	discontText,
+	totalCount,
+	buttonAddToCard,
+	closeBtn,
+	table,
+	addGood,
+	newGoodCount,
+	sumEl,
+	totalCountLine,
+	headTr,
+	totalCountMulti,
+};
